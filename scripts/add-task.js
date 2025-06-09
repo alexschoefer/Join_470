c780481eac456aac6dbb6e4aedc2d524abcee96f
Branislav Bardonbranislavbardon@hotmail.de <branislavbardon@hotmail.de>
2025-06-09 22:50:46 +0200

    feat: add-task.js made, function addTaskInit() onload of add-task.html,

---
+++ scripts/add-task.js
@@ -0,0 +1,17 @@
+let addTaskPrioButtonState = "";
+
+const atButtonPriorityUrgentRef = document.getElementById('add-task-priority-urgent');
+const atButtonPriorityMediumRef = document.getElementById('add-task-priority-medium');
+const atButtonPriorityLlowRef = document.getElementById('add-task-priority-low');
+
+function addTaskInit(){
+
+}
+
+function addTaskPrioButton(buttonType){
+    const buttonUrgent = atButtonPriorityUrgentRef;
+    const buttonMedium = atButtonPriorityMediumRef;
+    const buttonLow = atButtonPriorityLlowRef; 
+console.log(buttonType);
+
+}
