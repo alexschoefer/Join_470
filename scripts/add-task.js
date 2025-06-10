8ff08ae46632d77b6ac3b5f86f0c8027b5113196
Branislav Bardonbranislavbardon@hotmail.de <branislavbardon@hotmail.de>
2025-06-10 19:35:55 +0200

    feat: buttons :active added

--- scripts/add-task.js
+++ scripts/add-task.js
@@ -2,16 +2,34 @@ let addTaskPrioButtonState = "";
 
 const atButtonPriorityUrgentRef = document.getElementById('add-task-priority-urgent');
 const atButtonPriorityMediumRef = document.getElementById('add-task-priority-medium');
-const atButtonPriorityLlowRef = document.getElementById('add-task-priority-low');
+const atButtonPriorityLowRef = document.getElementById('add-task-priority-low');
 
-function addTaskInit(){
+
+function addTaskInit() {
 
 }
+// die buttons müssen dynamisch aus dem Template geladen werden !!!!
+function addTaskPrioButton(buttonType) {
+    if (!atButtonPriorityUrgentRef || !atButtonPriorityMediumRef || !atButtonPriorityLowRef) return;
 
-function addTaskPrioButton(buttonType){
-    const buttonUrgent = atButtonPriorityUrgentRef;
-    const buttonMedium = atButtonPriorityMediumRef;
-    const buttonLow = atButtonPriorityLlowRef; 
-console.log(buttonType);
-
+    if (buttonType === "urgent") {
+        atButtonPriorityUrgentRef.style.backgroundColor = "var(--button-urgent)";
+        atButtonPriorityMediumRef.style.backgroundColor = "var(--white)";
+        atButtonPriorityLowRef.style.backgroundColor = "var(--white)";
+        addTaskPrioButtonState = buttonType;
+    }
+    if (buttonType === "medium") {
+        atButtonPriorityUrgentRef.style.backgroundColor = "var(--white)";
+        atButtonPriorityMediumRef.style.backgroundColor = "var(--button-medium)";
+        atButtonPriorityLowRef.style.backgroundColor = "var(--white)";
+        addTaskPrioButtonState = buttonType;
+    }
+    if (buttonType === "low") {
+        atButtonPriorityUrgentRef.style.backgroundColor = "var(--white)";
+        atButtonPriorityMediumRef.style.backgroundColor = "var(--white)";
+        atButtonPriorityLowRef.style.backgroundColor = "var(--button-low)";
+        addTaskPrioButtonState = buttonType;
+    }
+    console.log(buttonType);
+    
 }
