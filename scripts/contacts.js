7b6fab9cf8ca8e65b1e3afb022b8b0ce5e3cbe21
alexschoefer <alex.schoefer@gmx.net>
2025-06-29 20:58:12 +0200

    feat: add new functions in contacts.js

--- scripts/contacts.js
+++ scripts/contacts.js
@@ -1,3 +1,29 @@
+let contacts = [];
+
+function getAllContacts() {
+    contactsDummy.forEach(contact => {
+        const { name, email, phone } = contact;
+        postContactsToRemoteStorage(name, email, phone);
+    });
+}
+
+async function postContactsToRemoteStorage(name, email, phone) {
+    let response = await fetch(fetchURLDataBase + '/contacts.json', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email,
+            phone: phone || ""
+        })
+    });
+
+    return await response.json();
+}
+
+
 function loadContacts(){
 
 }
