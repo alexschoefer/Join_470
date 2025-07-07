3fb6963004222fa9680c6e53de74d30032afa444
alexschoefer <alex.schoefer@gmx.net>
2025-07-07 22:13:34 +0200

    feat: working on editContactTemplate;

--- scripts/contacts.js
+++ scripts/contacts.js
@@ -139,7 +139,7 @@ function getContactInformations(index, event) {
     let selectedContact = document.getElementById('contact-details');
     selectedContact.innerHTML = "";
     toggleActiveContact(event.currentTarget);
-    selectedContact.innerHTML += showContactInformationsTemplate(contact);
+    selectedContact.innerHTML += showContactInformationsTemplate(contact,index);
 }
 
 function toggleActiveContact(element) {
@@ -157,7 +157,7 @@ function toggleActiveContact(element) {
     }
 }
 
-function showContactInformationsTemplate(contact) {
+function showContactInformationsTemplate(contact,index) {
     return `
     <div class="contact">
         <div class="contact-profil-badge">
@@ -166,7 +166,7 @@ function showContactInformationsTemplate(contact) {
         <div class="contact-profil">
             <div class="contact-profil-name">${contact.name}</div>
             <div class="contact-profil-btns-container">
-                <button class="contact-profil-btn-edit"><img src="../assets/icons/edit-icon.png" alt="edit-icon">Edit</button>
+                <button class="contact-profil-btn-edit" onclick="editContact(${index})"><img src="../assets/icons/edit-icon.png" alt="edit-icon">Edit</button>
                 <button class="contact-profil-btn-delete"><img src="../assets/icons/delete-icon.png" alt="">Delete</button>
             </div>
         </div>
@@ -176,18 +176,24 @@ function showContactInformationsTemplate(contact) {
         </div>
         <div class="contact-email-phone">
             <span>Email</span>
-            <div class="contact-email">${contact.email}</div>
+            <div class="contact-email">${contact.email || ''}</div>
             <span>Phone</span>
-            <div class="contact-phone">${contact.phone}</div>
+            <div class="contact-phone">${contact.phone || ''}</div>
         </div>
     `;
 }
 
-function editContact(contact) {
-    
+function editContact(index) {
+    const contact = allContacts[index];
+    let closeOverlay = document.getElementById('editContactOverlayContainer');
+    closeOverlay.classList.remove('d_none');
+    let overlayContainer = document.getElementById('editContactOverlayContainer');
+    overlayContainer.innerHTML = editContactTemplate(contact);
 }
 
 function addNewContact() {
+    let closeOverlay = document.getElementById('addNewContactOverlayContainer');
+    closeOverlay.classList.remove('d_none');
     let overlayContainer = document.getElementById('addNewContactOverlayContainer');
     overlayContainer.innerHTML = addNewContactTemplate();
 }
@@ -195,7 +201,11 @@ function addNewContact() {
 function addNewContactTemplate() {
     return `
 <div class="add-contact-overlay">
- <img class="add-contact-close-overlay-icon" src="../assets/icons/close.png" alt="close-icon">
+    <div>
+        <img class="add-contact-close-overlay-icon" src="../assets/icons/close.png" alt="close-icon"
+            onclick="closeAddContactOverlay()">
+    </div>
+
     <div class="add-contact-left-container">
         <img class="add-contact-menulogo" src="../assets/img/MenuLogo.png" alt="menulogo">
         <div class="add-contact-headline-container">
@@ -204,45 +214,145 @@ function addNewContactTemplate() {
             <div class="add-contact-vector-line"></div>
         </div>
     </div>
-    <div class="add-contact-right-container">
-        <div class="add-contact-information-wrapper">
-            <div class="add-contact-profil-icon">
-                <img src="../assets/icons/profil-icon.png" alt="profil-icon.png">
-            </div>
 
-            <div class="add-contact-input-wrapper">
+    <div class="add-contact-right-container">
+        <form class="add-contact-form" onsubmit="saveUserInputsForRemoteStorage(event)">
+            <div class="add-contact-information-wrapper">
+                <div class="add-contact-profil-icon">
+                    <img src="../assets/icons/profil-icon.png" alt="profil-icon.png">
+                </div>
+
+                <div class="add-contact-input-wrapper">
                     <div class="user-input-wrapper">
                         <div class="input-container">
-                            <input id="add-contact-name-input" class="user-input" type="text" name="name" placeholder="Name">
+                            <input id="add-contact-name-input" class="user-input" type="text" name="name"
+                                placeholder="Name" required>
                             <div>
-                                <img class="email-icon" src="../assets/icons/person-icon.png" alt="email-icon">
+                                <img class="email-icon" src="../assets/icons/person-icon.png" alt="person-icon">
                             </div>
                         </div>
                     </div>
 
-                <div class="user-input-wrapper">
+                    <div class="user-input-wrapper">
                         <div class="input-container">
-                            <input id="usermail-input" class="user-input" type="email" name="email"
-                                pattern="^[^\s@]+@[^\s@]+\.[^\s@]{2,}$" placeholder="Email" autocomplete="off"
-                                onfocus="clearErrorMessage(this)"
-                                onblur="validateSignupInput(this), checkRequiredInputEmail(this)">
+                            <input id="usermail-input" class="user-input" type="email" name="email" placeholder="Email"
+                                autocomplete="off" required pattern="^[^\\s@]+@[^\\s@]+\\.[^\\s@]{2,}$">
                             <div>
-                                <img class="email-icon" src="../assets/icons/mail-icon.png" alt="email-icon">
+                                <img class="email-icon" src="../assets/icons/mail-icon.png" alt="mail-icon">
                             </div>
                         </div>
                     </div>
 
-                <div class="user-input-wrapper">
+                    <div class="user-input-wrapper">
                         <div class="input-container">
-                            <input id="add-contact-phone-input" class="user-input" type="tel" name="phone" placeholder="Phone">
+                            <input id="add-contact-phone-input" class="user-input" type="tel" name="phone"
+                                placeholder="Phone" required>
                             <div>
-                                <img class="phone-icon" src="../assets/icons/call-icon.png" alt="phone-icon">
+                                <img class="phone-icon" src="../assets/icons/call-icon.png" alt="call-icon">
                             </div>
                         </div>
+                    </div>
+
+                    <div class="add-contact-btns-container">
+                        <button type="button" class="cancel-btn" onclick="closeAddContactOverlay()">
+                            Cancel
+                            <img class="add-task-button-create-icon" src="../assets/icons/close.png" alt="close-icon">
+                        </button>
+                        <button type="submit" class="create-contact-btn">
+                            Create contact
+                            <img class="add-task-button-check-icon" src="../assets/icons/check-icon.png"
+                                alt="check-icon">
+                        </button>
                     </div>
                 </div>
             </div>
+        </form>
+    </div>
+</div>
+    `
+}
+
+function bubblingPropagation(event) {
+    event.stopPropagation();
+}
+
+function closeAddContactOverlay() {
+    let closeOverlay = document.getElementById('addNewContactOverlayContainer');
+    closeOverlay.classList.add('d_none');
+}
+
+function closeEditContactOverlay() {
+    let closeOverlay = document.getElementById('editContactOverlayContainer');
+    closeOverlay.classList.add('d_none');
+}
+
+function editContactTemplate(contact) {
+    return `
+    <div class="add-contact-overlay">
+    <div>
+        <img class="add-contact-close-overlay-icon" src="../assets/icons/close.png" alt="close-icon"
+        onclick="closeEditContactOverlay()">
+    </div>
+
+    <div class="add-contact-left-container">
+        <img class="add-contact-menulogo" src="../assets/img/MenuLogo.png" alt="menulogo">
+        <div class="add-contact-headline-container">
+            <h3>Edit contact</h3>
+            <div class="add-contact-vector-line"></div>
         </div>
     </div>
+
+    <div class="add-contact-right-container">
+        <form class="add-contact-form" onsubmit="saveUserInputsForRemoteStorage(event)">
+            <div class="add-contact-information-wrapper">
+                <div class="add-contact-profil-icon">
+                    <div class="contact-details-icon" style="background-color: ${contact.profilcolor};">${contact.initial}</div>
+                </div>
+
+                <div class="add-contact-input-wrapper">
+                    <div class="user-input-wrapper">
+                        <div class="input-container">
+                            <input id="add-contact-name-input" class="user-input" type="text" name="name" placeholder="Name" value="${contact.name}">
+                            <div>
+                                <img class="email-icon" src="../assets/icons/person-icon.png" alt="person-icon">
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="user-input-wrapper">
+                        <div class="input-container">
+                            <input id="usermail-input" class="user-input" type="email" name="email" placeholder="Email"
+                                autocomplete="off" value="${contact.email}">
+                            <div>
+                                <img class="email-icon" src="../assets/icons/mail-icon.png" alt="mail-icon">
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="user-input-wrapper">
+                        <div class="input-container">
+                            <input id="add-contact-phone-input" class="user-input" type="tel" name="phone" placeholder="Phone"
+                                value="${contact.phone || ''}">
+                            <div>
+                                <img class="phone-icon" src="../assets/icons/call-icon.png" alt="call-icon">
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="add-contact-btns-container">
+                        <button type="button" class="delete-btn" onclick="closeAddContactOverlay()">
+                            Delete
+                        </button>
+                        <button type="submit" class="save-contact-btn">
+                            Save
+                            <img class="add-task-button-check-icon" src="../assets/icons/check-icon.png"
+                                alt="check-icon">
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
     `
 }
