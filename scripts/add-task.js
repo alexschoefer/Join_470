1855ea443249d59af2ec227082a4e492bc2cffaa
Branislav Bardonbranislavbardon@hotmail.de <branislavbardon@hotmail.de>
2025-07-09 22:22:10 +0200

    feat: when all required field are not empty, button create-task is enabled and becomes black color

--- scripts/add-task.js
+++ scripts/add-task.js
@@ -1,26 +1,15 @@
-const fetchURLDataBase =
-    "https://join-470-80a5e-default-rtdb.europe-west1.firebasedatabase.app/";
+const fetchURLDataBase = "https://join-470-80a5e-default-rtdb.europe-west1.firebasedatabase.app/";
 const ATTitleRef = document.getElementById("add-task-title-input");
-const ATDescriptionRef = document.getElementById(
-    "add-task-description-textarea"
-);
+const ATDescriptionRef = document.getElementById("add-task-description-textarea");
 const ATDueDateRef = document.getElementById("add-task-due-date-input");
-const ATButtonPrioButtonUrgentRef = document.getElementById(
-    "add-task-prio-button-urgent"
-);
-const ATButtonPrioButtonMediumRef = document.getElementById(
-    "add-task-prio-button-medium"
-);
-const ATButtonPrioButtonLowRef = document.getElementById(
-    "add-task-prio-button-low"
-);
+const ATButtonPrioButtonUrgentRef = document.getElementById("add-task-prio-button-urgent");
+const ATButtonPrioButtonMediumRef = document.getElementById("add-task-prio-button-medium");
+const ATButtonPrioButtonLowRef = document.getElementById("add-task-prio-button-low");
 const ATAssignToRef = document.getElementById("add-task-assign-to");
 const ATCategoryRef = document.getElementById("add-task-category");
 const ATSubtasksRef = document.getElementById("add-task-subtasks");
 const ATSubtaskInput = document.getElementById("add-task-subtasks-input");
-const ATButtonAddTaskRef = document.getElementById(
-    "add-task-button-create-task"
-);
+const ATButtonAddTaskRef = document.getElementById("add-task-button-create-task");
 const ATButtonCancelRef = document.getElementById("add-task-cancel-button");
 const allSubtasks = document.getElementById("allSubtasks");
 const ATButtonUrgentRef = document.getElementById('add-task-prio-button-urgent');
@@ -36,6 +25,11 @@ let subtasks = [];
 let subtasksObject = {};
 
 
+function addTaskInit() {
+    createTaskButtonRequiredFieldsNotOK();
+// contacts trigger
+}
+
 async function sendAddTaskData() {
     saveUserInputsForFirebase();
     resetAddTaskForm();
@@ -48,7 +42,6 @@ async function saveUserInputsForFirebase() {
     let date = ATDueDateRef.value;
     let priority = prioButtonState;
     let status = "toDo";
-
     let assignTo = "Branislav"; // assignedTo is a part of code, that has to be changed by dynamic version of contacts
     // let assignTo = ATAssignToRef.value;
     let category = ATCategoryRef.value;
@@ -63,7 +56,6 @@ async function saveUserInputsForFirebase() {
         category,
         subtasks
     );
-
 }
 
 async function checkIdAmount() {
@@ -140,15 +132,10 @@ function resetAddTaskForm() {
     ATDueDateRef.value = "";
     allSubtasks.innerHTML = "";
     ATSubtaskInput.value = "";
-
 }
 
 function addTaskPrioButtonClick(state) {
-    // if (state == prioButtonState) {
-    //     unholdPrioButtons();
-    //     prioButtonState = "";
-    // } else {
-    //     prioButtonState = state;
+
     if (state == 'Urgent') {
         holdButtonUrgent();
     }
@@ -159,13 +146,11 @@ function addTaskPrioButtonClick(state) {
         holdButtonLow();
     }
 }
-// }
 
 function holdButtonUrgent() {
     ATButtonLowPicRef.classList.remove('add-task-priority-button-low-pic-pressed');
     ATButtonMediumPicRef.classList.remove('add-task-priority-button-medium-pic-pressed');
     ATButtonUrgentPicRef.classList.add('add-task-priority-button-urgent-pic-pressed');
-
     ATButtonUrgentRef.classList.add('add-task-priority-button-urgent');
     ATButtonMediumRef.classList.remove('add-task-priority-button-medium');
     ATButtonLowRef.classList.remove('add-task-priority-button-low');
@@ -175,7 +160,6 @@ function holdButtonMedium() {
     ATButtonUrgentPicRef.classList.remove('add-task-priority-button-urgent-pic-pressed');
     ATButtonLowPicRef.classList.remove('add-task-priority-button-low-pic-pressed');
     ATButtonMediumPicRef.classList.add('add-task-priority-button-medium-pic-pressed');
-
     ATButtonUrgentRef.classList.remove('add-task-priority-button-urgent');
     ATButtonMediumRef.classList.add('add-task-priority-button-medium');
     ATButtonLowRef.classList.remove('add-task-priority-button-low');
@@ -185,24 +169,10 @@ function holdButtonLow() {
     ATButtonUrgentPicRef.classList.remove('add-task-priority-button-urgent-pic-pressed');
     ATButtonMediumPicRef.classList.remove('add-task-priority-button-medium-pic-pressed');
     ATButtonLowPicRef.classList.add('add-task-priority-button-low-pic-pressed');
-
     ATButtonUrgentRef.classList.remove('add-task-priority-button-urgent');
     ATButtonMediumRef.classList.remove('add-task-priority-button-medium');
     ATButtonLowRef.classList.add('add-task-priority-button-low');
 }
-
-// function unholdPrioButtons() {
-//     ATButtonUrgentPicRef.classList.remove('add-task-priority-button-urgent-pic-pressed');
-//     ATButtonMediumPicRef.classList.remove('add-task-priority-button-medium-pic-pressed');
-//     ATButtonLowPicRef.classList.remove('add-task-priority-button-low-pic-pressed');
-//     ATButtonUrgentRef.classList.remove('add-task-priority-button-urgent');
-//     ATButtonMediumRef.classList.remove('add-task-priority-button-medium');
-//     ATButtonLowRef.classList.remove('add-task-priority-button-low');
-// }
-
-
-
-
 
 function resetAddTaskSubtaskInput() {
     ATSubtaskInput.value = "";
@@ -315,7 +285,6 @@ function getDoneAddTaskSubtask(id) {
     const ATSubSubtaskIconEditRef = document.getElementById('add-task-subtasks-icon-edit-' + id);
     const ATSubSubtaskIconsRef = document.getElementById('add-task-subtasks-icons-' + id);
     const ATSubLiRef = document.getElementById('ATSubLi' + id);
-
     ATSubSubtaskContainerRef.blur();
     ATSubLiRef.classList.remove('d_none');
     ATSubSubtaskIconsRef.classList.remove('fdrr');
@@ -323,15 +292,12 @@ function getDoneAddTaskSubtask(id) {
     addTaskSubtasksIconDoneRef.classList.add('d_none');
 }
 
-
-
 function editAddTaskSubtask(id) {
     const ATSubSubtaskContainerRef = document.getElementById('ATSubtask-container-' + id);
     const ATSubSubtaskIconEditRef = document.getElementById('add-task-subtasks-icon-edit-' + id);
     const ATSubSubtaskIconsRef = document.getElementById('add-task-subtasks-icons-' + id);
     const addTaskSubtasksIconDoneRef = document.getElementById('add-task-subtasks-icon-done-' + id);
     const ATSubLiRef = document.getElementById('ATSubLi' + id);
-
     ATSubLiRef.classList.add('d_none');
     ATSubSubtaskContainerRef.focus();
     ATSubSubtaskIconsRef.classList.add('fdrr');
@@ -357,7 +323,6 @@ function getFocusInSubtasksInput() {
     ATSubtasksIconAddRef.classList.add('d_none');
 }
 
-
 function ADSShowIcons() {
     document.querySelectorAll('.add-task-subtask-style').forEach(container => {
         const icons = container.querySelector('.add-task-subtasks-icons');
@@ -379,3 +344,42 @@ document.getElementById('add-task-subtasks-input').addEventListener('keydown', f
         addTaskAddSubtask();
     }
 });
+
+function checkRequiredFieldsAndToggleButton() {
+    const titleFilled = ATTitleRef.value.trim() != "";
+    const dueDateFilled = ATDueDateRef.value.trim() != "";
+    const categoryFilled = ATCategoryRef.value.trim() != "";
+
+    if (titleFilled && dueDateFilled && categoryFilled) {
+        createTaskButtonRequiredFieldsOK();
+    } else {
+       createTaskButtonRequiredFieldsNotOK();
+    }
+}
+
+ATTitleRef.addEventListener('input', checkRequiredFieldsAndToggleButton);
+ATDueDateRef.addEventListener('input', checkRequiredFieldsAndToggleButton);
+ATCategoryRef.addEventListener('input', checkRequiredFieldsAndToggleButton);
+
+function createTaskButtonRequiredFieldsOK() {
+    ATButtonAddTaskRef.style.backgroundColor = "var(--black)";
+    ATButtonAddTaskRef.disabled = false;
+}
+
+function createTaskButtonRequiredFieldsNotOK() {
+     ATButtonAddTaskRef.style.backgroundColor = "var(--main-color)";
+        ATButtonAddTaskRef.disabled = true;
+}
+
+async function getContactsFromRemoteStorage() {
+    let response = await fetch(fetchURLDataBase + '/contacts' + '.json', {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+        }
+
+    });
+    const data = await response.json();
+    console.log(data);
+    return data;
+}
