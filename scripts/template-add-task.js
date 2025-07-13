aa6e88dae863a916261107ee0076568b1529e89e
Branislav Bardonbranislavbardon@hotmail.de <branislavbardon@hotmail.de>
2025-07-13 16:47:59 +0200

    feat: add-task Assgned to first concept of chosen profiles

--- scripts/template-add-task.js
+++ scripts/template-add-task.js
@@ -1,14 +1,17 @@
 
-function addSubtaskTemplate() {
-    return `<li id="add-task-subtask-template" class="add-task-subtask-style">
-                        <input id="ATSubtask-container" type="text" title="ATSubtask-container" class="ATSubtask-container"
-                     value="what ever you say!">
-                <div class="add-task-subtasks-icons" id="add-task-subtasks-icons">
-                     <div id="add-task-subtasks-icon-edit" class="add-task-subtasks-icon-edit" onclick="editAddTaskSubtask()">
+function addSubtaskTemplate(i, subtaski) {
+    return `<div id="add-task-subtask-template${i}" class="add-task-subtask-style">
+                <li class="ATSubLi" id="ATSubLi${i}"></li>                 <input id="ATSubtask-container-${i}" type="text" title="ATSubtask-container" class="ATSubtask-container"
+                     value="${subtaski}">
+                 <div class="add-task-subtasks-icons d_none" id="add-task-subtasks-icons-${i}">
+                     <div id="add-task-subtasks-icon-edit-${i}" class="add-task-subtasks-icon-edit" onclick="editAddTaskSubtask(${i})">
                      </div>
-                     <div id="add-task-subtasks-icons-divider" class="add-task-subtasks-icons-divider">
+                     <div id="add-task-subtasks-icon-done-${i}" class="add-task-subtasks-icon-done d_none" onclick="getDoneAddTaskSubtask(${i})">
                      </div>
-                     <div id="add-task-subtasks-icon-delete" class="add-task-subtasks-icon-delete" onclick="deleteAddTaskSubtask()">
+                     <div class="add-task-subtasks-icons-divider">
                      </div>
-                </div>
-             </li>`}
+                     <div id="add-task-subtasks-icon-delete-${i}" class="add-task-subtasks-icon-delete" onclick="deleteAddTaskSubtask(${i})">
+                     </div>
+                 </div>
+             </div>`;
+}
