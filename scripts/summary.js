41a3e4d8b8cad7dab039eba550560e43fe360fba
alexschoefer <alex.schoefer@gmx.net>
2025-08-15 18:48:36 +0200

    fix: time for greeting at the evening

--- scripts/summary.js
+++ scripts/summary.js
@@ -47,8 +47,10 @@ function setGreetingAndName() {
 function getCurrentGreeting() {
   const now = new Date();
   const hour = now.getHours();
+  console.log(hour);
+  
   if (hour <= 12) return "Good Morning";
-  if (hour <= 18) return "Good Afternoon";
+  if (hour <= 17) return "Good Afternoon";
   return "Good Evening";
 }
 
